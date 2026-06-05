ba7e0f19c6c7d31ad3c224d03c38f16e7eaef3ed
tufusa <me@tufusa.net>
2026-06-05 09:38:51 +0900

    chore: telemetry 系を exclude

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,7 @@ export const config = {
     { resource: { product: "workers", type: "scripts.versions" } },
     { resource: { product: "workers", type: "scripts.subdomain" } },
     { resource: { product: "workers", type: "scripts.script-settings" } },
+    { resource: { product: "workers", type: "observability.telemetry.keys" } },
+    { resource: { product: "workers", type: "observability.telemetry.query" } },
   ],
 };
